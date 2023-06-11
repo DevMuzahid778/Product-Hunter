19509cbf94b7c4815d74f042dfdabb6ebfc6d12e
Muzahid <mdmuzahid.gd@gmail.com>
2023-06-11 20:44:20 +0600

    error fixed

--- App.js
+++ App.js
@@ -4,21 +4,22 @@ function LoadData(Item){
     .then(res => res.json())
     .then(data => GetProducts(data.data))
 }
-    function GetProducts(data){
-        console.log(data)
-    }
 // when click the button- search value will set as LoadData parameter value for search item
 const Search = document.getElementById("Search")
 document.getElementById("SearchButton").addEventListener("click", function(){
-
+    
     if(Search.value != ""){
         LoadData(Search.value);
-
+        
         Search.value = ""
     }
     else{
         alert("please write the name of product in search field")
         Search.value =""
-
+        
     }
 })
+
+    function GetProducts(data){
+        console.log(data)
+    }
