c87e4e48dcf2dd37d0385010ef7bfe220c3b85e3
Muzahid <mdmuzahid.gd@gmail.com>
2023-06-11 22:10:08 +0600

    added the Product detail with modal

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ document.getElementById("SearchButton").addEventListener("click", function(){
     const ProductBody = document.getElementById("ProductBody")
     const SeeMoreBtn = document.getElementById("SeeMoreBtn")
     function GetProducts(Items){
-        console.log(Items)
+        // console.log(Items)
         if(Items.length > 7){
             SeeMoreBtn.classList.remove("d-none");
             Items.slice(0,7).forEach(Item => {
@@ -36,6 +36,7 @@ document.getElementById("SearchButton").addEventListener("click", function(){
                     <div class="card-body">
                         <h5 class="card-title">${Item.phone_name}</h5>
                     </div>
+                    <button onclick="GetDetails('${Item.slug}')" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">See Details</button>
                 </div>
                 `
 
@@ -53,6 +54,7 @@ document.getElementById("SearchButton").addEventListener("click", function(){
                         <div class="card-body">
                             <h5 class="card-title">${Item.phone_name}</h5>
                         </div>
+                        <button onclick="GetDetails('${Item.slug}')" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">See Details</button>
                     </div>
                     `
 
@@ -67,4 +69,37 @@ document.getElementById("SearchButton").addEventListener("click", function(){
         else{
             SeeMoreBtn.classList.add("d-none")
         }
+    }
+
+    function GetDetails(Id){
+        URl = `https://openapi.programming-hero.com/api/phone/${Id}`
+        fetch(URl)
+        .then(res => res.json())
+        .then(data => DisplayDetails(data.data))
+    }
+    
+    const ModalDialog = document.getElementById("ModalDialog");
+    function DisplayDetails(Details){
+        // console.log(Details)
+        // to clear before displayed modal
+        ModalDialog.innerHTML =""
+
+        const modalContent = document.createElement("div");
+        modalContent.classList.add("modal-content")
+        modalContent.innerHTML=`
+            <div class="modal-header">
+                <h1>${Details.name}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <p><h4>Release Date:</h4>${Details.releaseDate}</p>
+                <p><h5>Storage:</h5>${Details.mainFeatures.storage}</p>
+                <p><h5>Others:</h5>${Details.mainFeatures.sensors[0]}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        `
+        ModalDialog.appendChild(modalContent);
     }
