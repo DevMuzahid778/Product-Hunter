4debcbc8d461c5ac239a64cd2b72c91f8d0c1268
Muzahid <mdmuzahid.gd@gmail.com>
2023-06-11 20:42:35 +0600

    search Items features added

---
+++ App.js
@@ -0,0 +1,24 @@
+function LoadData(Item){
+    url =`https://openapi.programming-hero.com/api/phones?search=${Item}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => GetProducts(data.data))
+}
+    function GetProducts(data){
+        console.log(data)
+    }
+// when click the button- search value will set as LoadData parameter value for search item
+const Search = document.getElementById("Search")
+document.getElementById("SearchButton").addEventListener("click", function(){
+
+    if(Search.value != ""){
+        LoadData(Search.value);
+
+        Search.value = ""
+    }
+    else{
+        alert("please write the name of product in search field")
+        Search.value =""
+
+    }
+})
