b1777dd64ed5a50c4df64304ca5147a76755cefd
Muzahid <mdmuzahid.gd@gmail.com>
2023-06-11 21:25:56 +0600

    dynamic product added

--- App.js
+++ App.js
@@ -20,6 +20,51 @@ document.getElementById("SearchButton").addEventListener("click", function(){
     }
 })
 
-    function GetProducts(data){
-        console.log(data)
+// Now search the Product and display in HTML Page 
+    const ProductBody = document.getElementById("ProductBody")
+    const SeeMoreBtn = document.getElementById("SeeMoreBtn")
+    function GetProducts(Items){
+        console.log(Items)
+        if(Items.length > 7){
+            SeeMoreBtn.classList.remove("d-none");
+            Items.slice(0,7).forEach(Item => {
+                const ItemDIv = document.createElement("div");
+                ItemDIv.classList.add("col")
+                ItemDIv.innerHTML = `
+                <div class="card p-4">
+                    <img src="${Item.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${Item.phone_name}</h5>
+                    </div>
+                </div>
+                `
+
+                ProductBody.appendChild(ItemDIv)
+            });
+
+            SeeMoreBtn.addEventListener("click", function(){
+                Items.forEach(Item => {
+                    // console.log(Item)
+                    const ItemDIv = document.createElement("div");
+                    ItemDIv.classList.add("col")
+                    ItemDIv.innerHTML = `
+                    <div class="card p-4">
+                        <img src="${Item.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${Item.phone_name}</h5>
+                        </div>
+                    </div>
+                    `
+
+                    ProductBody.appendChild(ItemDIv)
+
+                    // to hide the see more button
+                    SeeMoreBtn.classList.add("d-none")
+                });
+            })
+
+        }
+        else{
+            SeeMoreBtn.classList.add("d-none")
+        }
     }
